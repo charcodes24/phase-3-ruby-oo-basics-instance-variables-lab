385f42a6ceb1305ae362340381d46221e63c04be
charcodes24 <charcodes24@gmail.com>
2021-07-14 10:51:18 -0700

    Completed assignment.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    #setter method
+    def name=(dog_name)
+        #assign the @instance var a value of whatever is passed as arg
+        @this_dogs_name = dog_name
+    end
 
+    #getter method
+    def name
+        #access the data from the @name instance var and retruns it
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
